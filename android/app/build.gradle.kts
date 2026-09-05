8fb7baecab99fb14b0b48fcb7aa6dc5c456eae6f
Abhi <abhigtm3003@gmail.com>
2026-09-05 23:31:56 +0530

    fix(build): force Kotlin stdlib 2.1.0 resolution strategy to resolve version conflict

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 }
@@ -8,6 +8,7 @@ plugins {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
 }
 
 android {
