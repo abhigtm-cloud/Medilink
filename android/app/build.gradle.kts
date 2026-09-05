338f1de4fa2aebbc6d4e83dfb20fda3782b3ea0e
Abhi <abhigtm3003@gmail.com>
2026-09-05 22:56:51 +0530

    fix(build): upgrade Kotlin Gradle Plugin to 2.1.0 and align JVM target to 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,14 +24,14 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 
